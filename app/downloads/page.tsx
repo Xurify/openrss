72d3510c5aa1f940b18c326c204fb4dec61609d5
Xurify <52548195+Xurify@users.noreply.github.com>
2025-02-09 04:13:27 -0500

    Added /downloads and /settings route

---
+++ app/downloads/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "OpenRSS - Downloads",
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Downloads</h1>
+    </>
+  );
+}
---
+++ app/settings/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "OpenRSS - Settings",
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>Settings</h1>
+    </>
+  );
+}
