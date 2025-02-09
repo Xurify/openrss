64686b590d5bb0f33b34937b0856f6191bdd57e4
Xurify <52548195+Xurify@users.noreply.github.com>
2025-02-09 03:58:19 -0500

    Removed irrelevant code

--- app/sw.ts
+++ app/sw.ts
@@ -21,50 +21,21 @@ self.addEventListener("activate", () => self.clients.claim());
 //const queue = new BackgroundSyncQueue("OpenRSS_Queue");
 
 const serwist = new Serwist({
-    precacheEntries: self.__SW_MANIFEST,
-    skipWaiting: true,
-    clientsClaim: true,
-    navigationPreload: true,
-    runtimeCaching: defaultCache,
-    fallbacks: {
-      entries: [
-        {
-          url: "/~offline",
-          matcher({ request }) {
-            return request.destination === "document";
-          },
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
+  fallbacks: {
+    entries: [
+      {
+        url: "/~offline",
+        matcher({ request }) {
+          return request.destination === "document";
         },
-      ],
-    },
-  });
-  
-const swr = new StaleWhileRevalidate();
-
-self.addEventListener("fetch", (event) => {
-  const { request } = event;
-  const url = new URL(request.url);
-  if (url.origin === location.origin && url.pathname === "/") {
-    event.respondWith(swr.handle({ event, request }));
-  }
+      },
+    ],
+  },
 });
-
-// self.addEventListener("fetch", (event) => {
-//     const url = new URL(event.request.url);
-//     // For "/legacy-post" with the method "POST", this simply makes a network request,
-//     // but if that fails due to a network problem, the request is added to the background
-//     // synchronization queue and will be retried later.
-//     if (event.request.method === "POST" && url.origin === location.origin && url.pathname === "/legacy-post") {
-//       const backgroundSync = async () => {
-//         try {
-//           const response = await fetch(event.request.clone());
-//           return response;
-//         } catch (error) {
-//           await queue.pushRequest({ request: event.request });
-//           return Response.error();
-//         }
-//       };
-//       event.respondWith(backgroundSync());
-//     }
-//   });
 
 serwist.addEventListeners();
