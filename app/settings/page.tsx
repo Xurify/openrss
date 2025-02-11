83739fe94f93cd1527c32d62f88e0f96de3b928d
Xurify <52548195+Xurify@users.noreply.github.com>
2025-02-11 14:46:23 -0500

    Fixed 'Do not pass children as props' error

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useState } from "react";
+import { useStore } from "@/contexts/StoreContext";
 import { Button } from "@/components/core/button";
 import { Modal } from "@/components/core/modal";
-import { useStore } from "@/contexts/StoreContext";
 
 export default function SettingsPage() {
   const { deleteEpisodes } = useStore();
@@ -18,10 +18,7 @@ export default function SettingsPage() {
   return (
     <div className="flex flex-col h-full w-full p-4">
       <h1 className="text-2xl font-semibold">Settings</h1>
-      <Button 
-        variant="destructive" 
-        onClick={() => setOpen(true)}
-      >
+      <Button variant="destructive" onClick={() => setOpen(true)}>
         Delete All Imported Feeds
       </Button>
       <Modal
@@ -32,24 +29,23 @@ export default function SettingsPage() {
         cancelText="Cancel"
         actionText="Delete Feeds"
         onAction={handleClearStorage}
-        children={
-          <div className="flex flex-col gap-4">
-            <label className="inline-flex items-center space-x-2">
-              <input
-                type="checkbox"
-                className="rounded border-gray-200 dark:bg-gray-800 dark:border-gray-700 ring-offset-white dark:ring-offset-gray-900 disabled:opacity-50 disabled:pointer-events-none"
-                checked={deleteFavorites}
-                onChange={(e) => setDeleteFavorites(e.target.checked)}
-                id="delete-favorites"
-              />
-              <span className="text-sm font-medium text-gray-700 dark:text-gray-300">
-                Remove favorites
-              </span>
-            </label>
-          </div>
-        }
         variant="destructive"
-      />
+      >
+        <div className="flex flex-col gap-4">
+          <label className="inline-flex items-center space-x-2">
+            <input
+              type="checkbox"
+              className="rounded border-gray-200 dark:bg-gray-800 dark:border-gray-700 ring-offset-white dark:ring-offset-gray-900 disabled:opacity-50 disabled:pointer-events-none"
+              checked={deleteFavorites}
+              onChange={(e) => setDeleteFavorites(e.target.checked)}
+              id="delete-favorites"
+            />
+            <span className="text-sm font-medium text-gray-700 dark:text-gray-300">
+              Remove favorites
+            </span>
+          </label>
+        </div>
+      </Modal>
     </div>
   );
-} 
+}
